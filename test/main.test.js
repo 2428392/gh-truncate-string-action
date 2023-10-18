f8221eeb72faedceefa4481938978c49846be402
Joseph T. Lyons <JosephTLyons@gmail.com>
2023-10-18 17:59:39 -0400

    Fix typos

--- test/main.test.js
+++ test/main.test.js
@@ -63,7 +63,7 @@ test.serial('returns non truncated string when string length is equal to max len
     await assertOutput(t.context.str, t);
 });
 
-test.serial('returns truncated string without dangaling hyphen', async (t) => {
+test.serial('returns truncated string without dangling hyphen', async (t) => {
     process.env.INPUT_STRINGTOTRUNCATE = t.context.strWithHyphenLessThanMax;
     process.env.INPUT_MAXLENGTH = 10;
     process.env.INPUT_REMOVEDANGLINGCHARACTERS = '-';
@@ -71,7 +71,7 @@ test.serial('returns truncated string without dangaling hyphen', async (t) => {
     await assertOutput('abcdefghi', t);
 });
 
-test.serial('returns truncated string when length = max length but has dangaling hyphen', async (t) => {
+test.serial('returns truncated string when length = max length but has dangling hyphen', async (t) => {
     process.env.INPUT_STRINGTOTRUNCATE = t.context.strWithHyphenAtMax;
     process.env.INPUT_MAXLENGTH = 27;
     process.env.INPUT_REMOVEDANGLINGCHARACTERS = '-';
